f414f9a3f2e9768dcc812cf653a0460cfbcf2756
Adam Aske <adamaske@live.no>
2020-11-10 03:22:11 +0100

    asd

--- Oblig3/MyHeader.h
+++ Oblig3/MyHeader.h
@@ -10,6 +10,30 @@ int p_x = 0;
 int p_y = 0;
 std::vector<int> enterdPassword;
 std::vector<int> correctPassword;
+std::vector<int> newPassword;
 bool Unlocked(int);
 void DrawPasswordBoard();
 void MainMenu();
+
+const int M = 6;
+const int N = 6;
+char board[M][N]; // 2d array with M rows and N columns // you can use vectors too
+int numberOfHits;
+int maxShots = 12;
+int shotsTaken;
+
+const char BLANK = ' ';
+const char SHIP = 'S';
+const char MISS = 'M';
+const char HIT = 'H';
+
+int letterToNumber(char);
+int RandomRow();
+int RandomColumn();
+void MakeBoard(int);
+void MakeBoardEmpty();
+void WriteLetters();
+void Shoot();
+void WriteLetters();
+void PrintBoard();
+void PrintPlayerBoard();
--- Oblig3/Oblig3.cpp
+++ Oblig3/Oblig3.cpp
@@ -7,10 +7,12 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <time.h>
 #include "MyHeader.h"
 
 int main()
 {
+    srand(time(0));
     if (Unlocked(3)) {
         MainMenu();
     }
@@ -24,6 +26,76 @@ void MainMenu() {
     system("cls");
     std::cout << "Welcome to Battleship!\n\n";
 
+    std::cout << "1. Change Password\n2.Play Battleship!\n3.Quit";
+
+    char input = _getch();
+    switch (input) {
+    case '1':
+        ChangePassword();
+        break;
+    case '2':
+        PlayBattleship();
+        break;
+    case '3':
+        return;
+        break;
+    default:
+        MainMenu();
+        break;
+    }
+}
+
+void ChangePassword() {
+    p_x = 0;
+    p_y = 0;
+    std::cout << "Enter your new password, [Enter] to comfirm!\n";
+
+    while (true) {
+        DrawPasswordBoard();
+        auto input = _getch();
+        if (input == 224) {
+            switch (_getch()) {
+            case KEY_UP:
+                if (p_x - 1 >= 0) {
+                    newPassword.push_back(KEY_UP);
+                    p_x--;
+                }
+                break;
+            case KEY_DOWN:
+                if (p_x + 1 <= 4) {
+                    newPassword.push_back(KEY_DOWN);
+                    p_x++;
+                }
+                break;
+            case KEY_RIGHT:
+                if (p_y + 1 <= 4) {
+                    newPassword.push_back(KEY_RIGHT);
+                    p_y++;
+                }
+                break;
+            case KEY_LEFT:
+                if (p_y - 1 >= 0) {
+                    newPassword.push_back(KEY_LEFT);
+                    p_y--;
+                }
+                break;
+            }
+        }
+        else if (input == '\r') {
+            //Check if its correct
+            //Create vector of correct password
+            std::fstream passwordFile("password.txt", std::ios::out);
+
+            for (int i = 0; i < newPassword.size()-1; i++)
+            {
+                passwordFile << newPassword[i] << std::endl;
+            }
+            std::cout << "\nYou have changed your password!\n\nPress any key to return to Main Menu...";
+            if (_getch()) {
+                MainMenu();
+            }
+        }
+    }
 
 }
 
@@ -162,5 +234,82 @@ void DrawPasswordBoard() {
         }
         std::cout << "\n";
     }
+}
 
+void PlayBattleship() 
+{
+    
+}
+void Shoot() {
+
+}
+
+void PrintBoard() {
+    for (int i = 0; i < M; i++)
+    {
+        std::cout << i << "|";
+        for (int k = 0; k < N; k++)
+        {
+            std::cout<< board[i][k] << "|";
+        }
+        std::cout << "\n";
+    }
+    
+}
+
+void PrintPlayerBoard() {
+    for (int i = 0; i < M; i++)
+    {
+        std::cout << i << "|";
+        for (int k = 0; k < N; k++)
+        {
+            //Dont print if its a ship;
+            if (board[i][k] != SHIP) {
+                std::cout << board[i][k] << "|";
+            }  
+        }
+        std::cout << "\n";
+    }
+}
+
+void MakeBoardEmpty() {
+    for (int i = 0; i < M; i++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            board[i][k] = BLANK;
+        }
+    }
+}
+
+void MakeBoard(int numberOfShips) {
+    int count = 0;
+    while (count < numberOfShips) {
+        int xpos = rand() % M;
+        int ypos = rand() % N;
+
+        if (board[xpos][ypos] == BLANK) {
+            board[xpos][ypos] == SHIP;
+            count++;
+        }
+    }
+}
+
+int letterToNumber(char letter) // only big letters
+{
+    return static_cast<int>(letter) - 65;
+}
+
+int RandomRow() {
+    return rand() % M;
+}
+int RandomColumn() {
+    return rand() % N;
+}
+void WriteLetters() {
+    std::cout << " |";
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << static_cast<char>(i) + 65 << "|";
+    }
 }
